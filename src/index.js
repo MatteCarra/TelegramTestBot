a1ac754b277d259a1ac9862160b9dee6ccd71b83
Matteo Carrara <matte.carra@gmail.com>
2018-09-13 23:17:17 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const { aws, telegram: { id: myId } } = require('../credentials.json');
-const dynamodb = new AWS.DynamoDB(aws);
 const app = express();
 const { sendMessage } = require("./TelegramApi.js");
 const { getSetup } = require('./tables/setup.js');
@@ -11,29 +10,32 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
 app.post('/', function (req, res) {
-  const { message_id, from, chat, date, text, new_chat_member } = req.body.message;
-  if(new_chat_member) {
-    const { id, first_name } = new_chat_member;
-    if(id === myId) {
-      sendMessage(chat.id, "Buongiorno, sono ScuolaBot!\r\nSpero di aiutarvi a superare al meglio l'anno scolastico, ricordandovi gli impegni scolastici e aiutandovi a fissare le interrogazioni programmate!\r\nPer iniziare mi servono un paio di informazioni.", { reply_markup: { InlineKeyboardMarkup: [{ text: "Test"}]}})
+  const { message, callback_query } = req.body;
+  if(message) {
+    const { message_id, from, chat, date, text, new_chat_member } = message;
+
+    if(new_chat_member) {
+      const { id, first_name } = new_chat_member;
+      if(id === myId) {
+        sendMessage(chat.id, "Buongiorno, sono ScuolaBot!\r\nSpero di aiutarvi a superare al meglio l'anno scolastico, ricordandovi gli impegni scolastici e aiutandovi a fissare le interrogazioni programmate!\r\nPer iniziare mi servono un paio di informazioni.", { reply_markup: { inline_keyboard: [{ text: "Test"}]}})
+      } else {
+        sendMessage(chat.id, `Benvenuto/a nella classe ${first_name}!`)
+      }
+    } else if(text) {
+      if(chat.id === from.id) { //private chat
+        //Todo I've to get if this user is part of a classe. If he is I'm getting his class and showing
+      } else { //group
+
+      }
     } else {
-      sendMessage(chat.id, `Benvenuto/a nella classe ${first_name}!`)
+      console.log(req.body)
     }
-  } else if(text) {
-    if(chat.id === from.id) { //private chat
-      //Todo I've to get if this user is part of a classe. If he is I'm getting his class and showing
-    } else { //group
-      getSetup(chat.id)
-        .then(setup => { //We have a setup running
-
-        })
-        .catch(err => { //We don't have a setup running. Handling commands regularly.
-
-        })
-    }
+  } else if(callback_query) {
+    sendMessage("Hey, nice pick!")
   } else {
     console.log(req.body)
   }
+
   return res.json({})
 });
 
